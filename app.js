60db784772c6c69b9c9314745a1cb809efc454c4
DESKTOP-GUGSNQ4 <saqlainzarjisansari@gmail.com>
2024-07-20 11:17:48 +0530

    feat: adding in authorization

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ const catchAsync = require('./utils/catchAsync');
 const Review =  require('./models/review')
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/review')
+const reviews = require('./routes/review');
+const passport = require('passport');
+const userRoutes = require('./routes/users')
+const User = require('./models/user')
+const LocalStrategy = require('passport-local');
 const db = mongoose.connection;
 const session = require('express-session');
 db.on("error",console.error.bind(console,"connection error:"));
@@ -42,18 +46,21 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig))
 app.use(flash());
-
-
-
-
+app.use(passport.initialize())
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 // we have deleted review but what if we dlt a whole card review still stays in the database
 // we need a monoogse midde ware to dlt this
 app.use((req, res, next) => {
+    console.log(req.session)
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
-
+app.use('/',userRoutes);
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
 
